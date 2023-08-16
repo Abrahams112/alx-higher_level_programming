6153691103944706b0e47d613917e01e329f5994
abrahams112 <abrahamgideon11@gmail.com>
2023-08-16 20:59:36 +0000

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,10 +2,8 @@
 #!/usr/bin/python3
 
 # recieve 2 interger and add them together and print result 
-num1, operator, num2 = Results
+num1, operator, num2 = Results input ("enter values").split()
 	
 a = num1
 b = num2
-num1 = 1
-num2 = 2
 print("{} + {} = {}".format(num1, mum2, num1+num2))
