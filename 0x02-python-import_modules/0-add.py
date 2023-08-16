55504d4812a385f58da1720ae0fe77dee9c25c46
abrahams112 <abrahamgideon11@gmail.com>
2023-08-16 20:56:07 +0000

    add

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,11 @@
+
+#!/usr/bin/python3
+
+# recieve 2 interger and add them together and print result 
+num1, operator, num2 = Results
+	
+a = num1
+b = num2
+num1 = 1
+num2 = 2
+print("{} + {} = {}".format(num1, mum2, num1+num2))
